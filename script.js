45bb8f14b7bc83f4239db7ef1a35211cd8dbd016
unknown <emadeldeen.lina@gmail.com>
2023-05-24 17:45:34 +0300

    Full Project

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const buttonClose = document.querySelector(".close-modal");
+const buttonShow = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < buttonShow.length; i++) {
+  buttonShow[i].addEventListener("click", openModal);
+}
+
+buttonClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
